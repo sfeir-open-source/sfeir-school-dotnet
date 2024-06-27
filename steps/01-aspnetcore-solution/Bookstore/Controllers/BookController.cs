33569a9eaa2d832f5018aee85e2b085e4162b1c3
BUCLON Joany <buclon.j@sfeir.com>
2024-06-27 17:44:23 +0200

    feat: add aspnetcore basic step

---
+++ steps/01-aspnetcore-solution/Bookstore/Controllers/BookController.cs
@@ -0,0 +1,48 @@
+﻿using Bookstore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookstore.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BookController : ControllerBase
+    {
+        private readonly List<Book> _bookstore;
+
+        public BookController()
+        {
+            _bookstore = [
+                new Book { Id = 0, Title = "Neuromancer", Author = "William Gibson", Price = 10, Tags = ["SF", "Cyberpunk"] },
+                new Book { Id = 1, Title = "Dune", Author = "Franck Herbert", Price = 15, Tags = ["SF", "Politics"] },
+                new Book { Id = 1, Title = "Foundation", Author = "Isaac Asimov", Price = 9, Tags = ["SF", "Politics"] },
+                new Book { Id = 1, Title = "3 Body Problem", Author = "Liu Cixin", Price = 11, Tags = ["SF", "Mystery"] },
+            ];
+        }
+
+        [HttpPost]
+        public void Post(Book book)
+        {
+            _bookstore.Add(book);
+        }
+
+        [HttpGet("{id}")]
+        public Book Get(int id)
+        {
+            return _bookstore.First(w => w.Id == id);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var bookToRemove = _bookstore.First(w => w.Id == id);
+            _bookstore.Remove(bookToRemove);
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, Book book)
+        {
+            _bookstore.FindIndex(w => w.Id == id);
+            _bookstore[id] = book;
+        }
+    }
+}
---
+++ steps/01-aspnetcore-solution/Bookstore/Models/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace Bookstore.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public float Price { get; set; }
+        public string Author { get; set; }
+        public List<string> Tags { get; set; }
+    }
+}
