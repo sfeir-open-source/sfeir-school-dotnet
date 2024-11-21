dc5890de925430d36194fbe29765d608a7cd829f
BUCLON Joany <buclon.j@sfeir.com>
2024-11-21 17:47:40 +0100

    feat: updatge API with ActionResults

--- steps/02-ajout-swagger-solution/Bookstore/Controllers/BookController.cs
+++ steps/02-ajout-swagger-solution/Bookstore/Controllers/BookController.cs
@@ -1,48 +1,62 @@
-﻿using Bookstore.Models;
+using Bookstore.Models;
+
 using Microsoft.AspNetCore.Mvc;
 
-namespace Bookstore.Controllers
+namespace Bookstore.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class BookController : ControllerBase
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class BookController : ControllerBase
-    {
-        private readonly List<Book> _bookstore;
+  private readonly List<Book> _bookstore;
 
-        public BookController()
-        {
-            _bookstore = [
-                new Book { Id = 0, Title = "Neuromancer", Author = "William Gibson", Price = 10, Tags = ["SF", "Cyberpunk"] },
-                new Book { Id = 1, Title = "Dune", Author = "Franck Herbert", Price = 15, Tags = ["SF", "Politics"] },
-                new Book { Id = 2, Title = "Foundation", Author = "Isaac Asimov", Price = 9, Tags = ["SF", "Politics"] },
-                new Book { Id = 3, Title = "3 Body Problem", Author = "Liu Cixin", Price = 11, Tags = ["SF", "Mystery"] },
-            ];
-        }
+  public BookController()
+  {
+    _bookstore = [
+        new Book { Id = 0, Title = "Neuromancer", Author = "William Gibson", Price = 10, Tags = ["SF", "Cyberpunk"] },
+        new Book { Id = 1, Title = "Dune", Author = "Franck Herbert", Price = 15, Tags = ["SF", "Politics"] },
+        new Book { Id = 2, Title = "Foundation", Author = "Isaac Asimov", Price = 9, Tags = ["SF", "Politics"] },
+        new Book { Id = 3, Title = "3 Body Problem", Author = "Liu Cixin", Price = 11, Tags = ["SF", "Mystery"] },
+    ];
+  }
 
-        [HttpPost]
-        public void Post(Book book)
-        {
-            _bookstore.Add(book);
-        }
+  [HttpPost]
+  public IActionResult Post(Book book)
+  {
+    if (book.Title is null || book.Author is null) return BadRequest();
+    _bookstore.Add(book);
+    return Created("", book);
+  }
 
-        [HttpGet("{id}")]
-        public Book Get(int id)
-        {
-            return _bookstore.First(w => w.Id == id);
-        }
+  [HttpGet]
+  public IActionResult Get()
+  {
+    if (_bookstore.Count > 0) return Ok(_bookstore);
+    return NotFound();
+  }
 
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-            var bookToRemove = _bookstore.First(w => w.Id == id);
-            _bookstore.Remove(bookToRemove);
-        }
+  [HttpGet("{id}")]
+  public IActionResult Get(int id)
+  {
+    var result = _bookstore.Find(w => w.Id == id);
+    if (result is null) return NotFound();
+    return Ok(result);
+  }
 
-        [HttpPut("{id}")]
-        public void Put(int id, Book book)
-        {
-            _bookstore.FindIndex(w => w.Id == id);
-            _bookstore[id] = book;
-        }
-    }
+  [HttpDelete("{id}")]
+  public IActionResult Delete(int id)
+  {
+    var bookToRemove = _bookstore.Find(w => w.Id == id);
+    if (bookToRemove is null) return NotFound();
+    _bookstore.Remove(bookToRemove);
+    return NoContent();
+  }
+
+  [HttpPut("{id}")]
+  public IActionResult Put(int id, Book book)
+  {
+    if (!_bookstore.Exists(w => w.Id == id)) return NotFound();
+    _bookstore[id] = book;
+    return Ok(book);
+  }
 }
--- steps/02-ajout-swagger-solution/Bookstore/Models/Book.cs
+++ steps/02-ajout-swagger-solution/Bookstore/Models/Book.cs
@@ -1,11 +1,10 @@
-namespace Bookstore.Models
+namespace Bookstore.Models;
+
+public class Book
 {
-    public class Book
-    {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public float Price { get; set; }
-        public string Author { get; set; }
-        public List<string> Tags { get; set; }
-    }
+  public int Id { get; set; }
+  public required string Title { get; set; }
+  public float Price { get; set; }
+  public required string Author { get; set; }
+  public List<string> Tags { get; set; } = [];
 }
